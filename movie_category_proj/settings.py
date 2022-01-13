0081b8c9bb9201cc2787408ff9c2191a60216e9c
AlexandrMarchevskiy <90780273+AlexandrMarchevskiy@users.noreply.github.com>
2022-01-13 17:04:46 +0300

    Update settings.py

--- movie_category_proj/settings.py
+++ movie_category_proj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&12r^yao7ihg!p04^+td^vey8n+u8b-u64hv6tmg(9srj)0jer'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
